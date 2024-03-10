a6b5fb9d20db95fe0f61b8364d05971dd2341dfd
Landibtw <landixbtw@gmail.com>
2024-03-10 18:05:30 +0100

    Changes colorscheme, added lazy load to LazyGit plugin

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -2,6 +2,7 @@ return {
   -- nvim v0.8.0
   {
     'kdheepak/lazygit.nvim',
+    lazy = false,
     cmd = {
       'LazyGit',
     },
@@ -10,7 +11,7 @@ return {
       'nvim-lua/plenary.nvim',
     },
     config = function()
-      -- Lazygit Keymaps
+      -- Lazygit Keymap
       vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { silent = true, desc = 'Open LazyGit' })
     end,
   },
