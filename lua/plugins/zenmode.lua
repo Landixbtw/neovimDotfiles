b154e48de3da12d098ca20a0ae756e005f9838c2
Landibtw <landixbtw@gmail.com>
2024-03-15 10:36:54 +0100

    added spectre and zenmode plugins

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,33 @@
+return {
+  -- Lua
+  {
+    'folke/zen-mode.nvim',
+
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      window = {
+        backdrop = 0.95,
+        width = 120,
+        height = 1,
+      },
+      plugins = {
+        options = {
+          cursorline = false,
+        },
+        twilight = { enabled = true }, -- enable to start with Twilight
+        git = { enabled = true }, -- enables gitsigns
+        tmux = { enabled = false }, --disables tmux statusline
+        alacritty = {
+          enabled = true,
+          font = '13', -- font size increases to 13
+        },
+      },
+    },
+    config = function()
+      -- ZenMode Keymap
+      vim.keymap.set('n', '<leader>z', ':ZenMode<CR>', { silent = true, desc = 'Toggle ZenMode' })
+    end,
+  },
+}
