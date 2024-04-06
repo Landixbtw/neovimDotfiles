66bb6152dcf68abc7e12fe52742851df8dbc0a59
Landibtw <landixbtw@gmail.com>
2024-04-06 21:27:11 +0200

    added some to desc

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -3,5 +3,6 @@ return {
     'f-person/git-blame.nvim',
     event = 'VeryLazy',
   },
-  vim.keymap.set('n', '<leader>gbo', ':GitBlameOpenCommitURL<CR>', { desc = 'Open the git commit url' }),
+  vim.keymap.set('n', '<leader>gbo', ':GitBlameOpenCommitURL<CR>', { desc = 'Open the git commit in chrome' }),
+  -- Opens commit in chrome
 }
