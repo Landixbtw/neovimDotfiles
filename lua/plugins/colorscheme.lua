cbed41eeb152eef885d90b9693ec5b39a870a15b
Landibtw <landixbtw@gmail.com>
2024-04-15 11:09:15 +0200

    colorscheme update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,39 +1,48 @@
+function ColorMyPencils(color)
+  color = color or 'rose-pine'
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    --'neanias/everforest-nvim',
-    --'ribru17/bamboo.nvim',
-    --'NLKNguyen/papercolor-theme', -- Name, "PaperColor"
-    --'olivercederborg/poimandres.nvim',
-    -- default colorscheme would be folke/tokyonight.nvim
-    --'folke/tokyonight.nvim',
-    --'aktersnurra/no-clown-fiesta.nvim',
-    --'felipeagc/fleet-theme-nvim',
-    --'LunarVim/templeos.nvim',
-    --   'namrabtw/rusty.nvim',
-    'rebelot/kanagawa.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    --name = 'biscuit',
-    dependencies = {
-      'rktjmp/lush.nvim',
-    },
+  {
+    'folke/tokyonight.nvim',
     config = function()
-      --vim.g.zenbones_compat = 1
-      -- require('poimandres').setup {
-      --   bold_vert_split = false,
-      --   disable_background = true,
-      --   disable_float_background = true,
-      -- }
+      require('tokyonight').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
+        },
+      }
+    end,
+  },
 
-      -- Load the colorscheme here
-      vim.cmd.colorscheme 'kanagawa'
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        disable_background = false,
+        styles = {
+          italic = false,
+        },
+      }
 
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd 'colorscheme rose-pine'
+
+      ColorMyPencils()
     end,
   },
 }
